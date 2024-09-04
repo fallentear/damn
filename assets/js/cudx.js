4b402220de8ce307773fdc01bb2352b4d59bd4a0
tears <shadowsweatuk@gmail.com>
2024-09-04 03:13:47 -0700

    Update cudx.js

--- assets/js/cudx.js
+++ assets/js/cudx.js
@@ -1,55 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const x = document.getElementById("enter");
+  const x = document.getElementById("enter");
 
-    function xx() {
-        document.fullscreenElement ? document.xxx && document.xxx() : document.documentElement.requestFullscreen();
-    }
+  function xx() {
+      document.fullscreenElement ? document.xxx && document.xxx() : document.documentElement.requestFullscreen();
+  }
 
-    x.addEventListener("click", xx);
+  x.addEventListener("click", xx);
 
-    const body = document.getElementsByTagName("body")[0];
+  const body = document.getElementsByTagName("body")[0];
 
-    window.addEventListener("keydown", (e) => {
-        if (e.ctrlKey) {
-            switch (e.key.toLowerCase()) {
-                case "s":
-                    e.preventDefault();
-                    window.location.href = "https://bloody.bio/consist";
-                    break;
-                case "c":
-                    e.preventDefault();
-                    window.location.href = "https://bloody.bio/consist";
-                    break;
-                case "e":
-                    e.preventDefault();
-                    window.location.href = "https://bloody.bio/consist";
-                    break;
-                case "u":
-                    e.preventDefault();
-                    window.location.href = "https://bloody.bio/consist";
-                    break;
-            }
-        }
-    });
+  window.addEventListener("keydown", (e) => {
+      if (e.ctrlKey) {
+          switch (e.key.toLowerCase()) {
+              case "s":
+                  e.preventDefault();
+                  window.location.href = "https://resort.lol/1";
+                  break;
+              case "c":
+                  e.preventDefault();
+                  window.location.href = "https://resort.lol/1";
+                  break;
+              case "e":
+                  e.preventDefault();
+                  window.location.href = "https://resort.lol/1";
+                  break;
+              case "u":
+                  e.preventDefault();
+                  window.location.href = "https://resort.lol/1";
+                  break;
+          }
+      }
+  });
 
-    document.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-    });
+  document.addEventListener("contextmenu", function (e) {
+      e.preventDefault();
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    var options = {
-        strings: ["domain owner @ resort.lol", "restrict my left hand", "@cudip on ts", ":3", "aint nobody like this mf"],
-        typeSpeed: 45,
-        backSpeed: 35,
-        backDelay: 500,
-        startDelay: 500,
-        loop: true,
-        showCursor: false 
-    };
-    
-    var typed = new Typed(".description", options);
-    
+  var options = {
+      strings: ["domain owner @ resort.lol", "restrict my left hand", "@mr.gripha on instagram", ":3", "aint nobody like this mf"],
+      typeSpeed: 45,
+      backSpeed: 35,
+      backDelay: 500,
+      startDelay: 500,
+      loop: true,
+      showCursor: false 
+  };
+  
+  var typed = new Typed(".description", options);
+  
 });
